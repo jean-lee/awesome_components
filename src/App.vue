9050a40dce5c33bd21a3e0b16c58dab6c4d7eb5c
jean-lee <1486075310@qq.com>
2019-11-05 18:28:04 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue {
+  private mounted() {
+    (document.getElementById('app') as HTMLElement).style.display = 'block';
+    (document.getElementById('appLoading') as HTMLElement).style.display = 'none';
+  }
+}
+</script>
+
+
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+
+<style lang="stylus">
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({
+  name: 'home',
+})
+export default class Home extends Vue {
+
+}
+</script>
+
+
+<template>
+  <div class="module_home_index">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
